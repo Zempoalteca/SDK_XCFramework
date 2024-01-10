38bf12c8883c471078d56fc65febaba1f5d25527
Gabriel Zempoalteca <gabriel.zmpltc@gmail.com>
2024-01-10 11:58:15 -0600

    The whole framework

---
+++ MyLibraryTest.xcframework/ios-arm64_x86_64-simulator/MyLibraryV.framework/Headers/MyLibraryV.h
@@ -0,0 +1,18 @@
+//
+//  MyLibraryV.h
+//  MyLibraryV
+//
+//  Created by Gabriel Zempoalteca Garrido on 04/01/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyLibraryV.
+FOUNDATION_EXPORT double MyLibraryVVersionNumber;
+
+//! Project version string for MyLibraryV.
+FOUNDATION_EXPORT const unsigned char MyLibraryVVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyLibraryV/PublicHeader.h>
+
+
